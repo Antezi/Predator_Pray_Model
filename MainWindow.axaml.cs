6c11bf780644a5ffdadac367a11bb3f237399acc
Antezi <swiatkin2019@yandex.ru>
2024-02-24 00:23:27 +0300

    4rd commit

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -6,155 +6,153 @@ using OxyPlot;
 using OxyPlot.Avalonia;
 using LineSeries = OxyPlot.Series.LineSeries;
 
-namespace Predator_Pray_Model;
 
-public partial class MainWindow : Window
+namespace Predator_Pray_Model
 {
-    private TextBox _prayBirth, _predatorDeath, _killingBox, _deathPrayBox;
-    private double e, B, a, d, prayCount, predatorCount;
-    private int iter = 0;
-    private int gen = 0;
-    private List<double> prayCountList = new List<double>();
-    private List<double> predatorCountList = new List<double>();
-    private double startPrayCount = 500, startPredatorCount = 75;
-    private double lastPray, lastPredator;
-    private PlotModel mainPlotModel, phasePlotModel;
-    private PlotView _plotModel, _phaseModel;
-
-    public MainWindow()
+    public partial class MainWindow : Window
     {
+        private TextBox _prayBirth, _predatorDeath, _killingBox, _deathPrayBox;
+        private double e, B, a, d, prayCount, predatorCount;
+        private int iter = 0;
+        private List<double> prayCountList = new List<double>();
+        private List<double> predatorCountList = new List<double>();
+        private double[,] prayCounts, predatorCounts;
+        private double startPrayCount = 1000, startPredatorCount = 100;
+        private double lastPray, lastPredator;
+        private PlotModel mainPlotModel, phasePlotModel;
+        private PlotView _plotModel, _phaseModel;
+        private LineSeries predatorsSeries, preysSeries, phaseSeries;
 
-        MinHeight = 450;
-        MaxHeight = 450;
-        MinWidth = 1200;
-        MaxWidth = 1200;
-
-        InitializeComponent();
-
-        _prayBirth = this.FindControl<TextBox>("PrayBirth");
-        _predatorDeath = this.FindControl<TextBox>("PredatorDeath");
-        _killingBox = this.FindControl<TextBox>("KillingBox");
-        _deathPrayBox = this.FindControl<TextBox>("DeathPrayBox");
-        _plotModel = this.FindControl<PlotView>("MainPlot");
-        _phaseModel = this.FindControl<PlotView>("PhasePlot");
-        
-        mainPlotModel = CreateMainPlotModel();
-        phasePlotModel = CreatePhasePlotModel();
-        MainPlot.Model = mainPlotModel;
-        PhasePlot.Model = phasePlotModel;
-    }
-
-    private void GenerateButton_OnClick(object? sender, RoutedEventArgs e)
-    {
-        try
+        public MainWindow()
         {
-            this.e = Convert.ToDouble(_prayBirth.Text);
-            B = Convert.ToDouble(_predatorDeath.Text);
-            a = Convert.ToDouble(_killingBox.Text);
-            d = Convert.ToDouble(_deathPrayBox.Text);
-        }
-        catch
-        {
-            return;
+            MinHeight = 450;
+            MaxHeight = 450;
+            MinWidth = 1200;
+            MaxWidth = 1200;
+
+            InitializeComponent();
+
+            _prayBirth = this.FindControl<TextBox>("PrayBirth");
+            _predatorDeath = this.FindControl<TextBox>("PredatorDeath");
+            _killingBox = this.FindControl<TextBox>("KillingBox");
+            _deathPrayBox = this.FindControl<TextBox>("DeathPrayBox");
+            _plotModel = this.FindControl<PlotView>("MainPlot");
+            _phaseModel = this.FindControl<PlotView>("PhasePlot");
+
+            mainPlotModel = CreateMainPlotModel();
+            phasePlotModel = CreatePhasePlotModel();
+            MainPlot.Model = mainPlotModel;
+            PhasePlot.Model = phasePlotModel;
         }
 
-        // Количество жертв
-        for (int j = 0; j < 3; j++)
+        private void GenerateButton_OnClick(object? sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 150; i++)
+            try
             {
-                if (gen > 0 && i == 0)
-                {
-                    prayCount = (this.e - a * lastPredator) * lastPray + lastPray;
-                    iter++;
-                    prayCountList.Add(prayCount);
-                }
-                else if (iter == 0)
-                {
-                    prayCount = (this.e - a * startPredatorCount) * startPrayCount + startPrayCount;
-                    iter++;
-                    prayCountList.Add(prayCount);
-                }
-                else
-                {
-                    prayCount = (this.e - a * predatorCount) * prayCount + prayCount;
-                    iter++;
-                    prayCountList.Add(prayCount);
-                }
+                this.e = Convert.ToDouble(_prayBirth.Text);
+                B = Convert.ToDouble(_predatorDeath.Text);
+                a = Convert.ToDouble(_killingBox.Text);
+                d = Convert.ToDouble(_deathPrayBox.Text);
+            }
+            catch
+            {
+                return;
+            }
 
-                if (gen > 0 && i == 0)
+            prayCounts = new double[3, 150];
+            predatorCounts = new double[3, 150];
+            // Количество жертв
+            for (int j = 0; j < 3; j++)
+            {
+                for (int i = 0; i < 150; i++)
                 {
-                    predatorCount = (d * prayCountList[i] - B) * lastPredator + lastPredator;
-                    predatorCountList.Add(predatorCount);
-                }
-                else if (iter == 1)
-                {
-                    predatorCount = (d * prayCountList[i] - B) * startPredatorCount + startPredatorCount;
-                    predatorCountList.Add(predatorCount);
-                }
+                    if (j > 0 && i == 0)
+                    {
+                        prayCount = (this.e - a * lastPredator) * lastPray + lastPray;
+                        iter++;
+                        prayCounts[j, i] = prayCount;
+                    }
+                    else if (iter == 0)
+                    {
+                        prayCount = (this.e - a * startPredatorCount) * startPrayCount + startPrayCount;
+                        iter++;
+                        prayCounts[j, i] = prayCount;
+                    }
+                    else
+                    {
+                        prayCount = (this.e - a * predatorCount) * prayCount + prayCount;
+                        iter++;
+                        prayCounts[j, i] = prayCount;
+                    }
 
-                else
-                {
-                    predatorCount = (d * prayCountList[i] - B) * predatorCount + predatorCount;
-                    predatorCountList.Add(predatorCount);
-                }
+                    if (j > 0 && i == 0)
+                    {
+                        predatorCount = (d * prayCounts[j, i] - B) * lastPredator + lastPredator;
+                        predatorCounts[j, i] = predatorCount;
+                    }
+                    else if (iter == 1)
+                    {
+                        predatorCount = (d * prayCounts[j, i] - B) * startPredatorCount + startPredatorCount;
+                        predatorCounts[j, i] = predatorCount;
+                    }
 
-                if (i == 149)
+                    else
+                    {
+                        predatorCount = (d * prayCounts[j, i] - B) * predatorCount + predatorCount;
+                        predatorCounts[j, i] = predatorCount;
+                    }
+
+                    if (i == 149)
+                    {
+                        lastPray = prayCounts[j, i];
+                        lastPredator = predatorCounts[j, i];
+                        //prayCounts[j, i] = prayCount;
+                        //predatorCounts[j, i] = predatorCount;
+                    }
+                }
+            }
+
+            UpdatePlotData();
+        }
+
+        private void UpdatePlotData()
+        {
+            predatorsSeries.Points.Clear();
+            preysSeries.Points.Clear();
+            phaseSeries.Points.Clear();
+
+            int gen = 0;
+            for (int j = 0; j < 3; j++)
+            {
+                for (int i = 0; i < 150; i++)
                 {
-                    lastPray = prayCountList[i];
-                    lastPredator = predatorCountList[i];
+                    predatorsSeries.Points.Add(new DataPoint(gen, predatorCounts[j, i]));
+                    preysSeries.Points.Add(new DataPoint(gen, prayCounts[j, i]));
+                    phaseSeries.Points.Add(new DataPoint(predatorCounts[j, i], prayCounts[j, i]));
                     gen++;
                 }
             }
-            UpdatePlotData();
-            prayCountList = new List<double>();
-            predatorCountList = new List<double>();
+
+            _plotModel.InvalidatePlot();
+            _phaseModel.InvalidatePlot();
         }
-        
-    }
-    
-    private void UpdatePlotData()
-    {
-        //mainPlotModel.Series.Clear();
-        //phasePlotModel.Series.Clear();
 
-        var predatorsSeries = new LineSeries { Title = "Predators" };
-        var preysSeries = new LineSeries { Title = "Preys" };
-        var phaseSeries = new LineSeries { Title = "Predators vs Preys" };
-
-        // Добавляем данные из prayCountList и predatorCountList в серии для основной диаграммы
-        for (int i = 0; i < prayCountList.Count; i++)
+        private PlotModel CreateMainPlotModel()
         {
-            predatorsSeries.Points.Add(new DataPoint(i, predatorCountList[i]));
-            preysSeries.Points.Add(new DataPoint(i, prayCountList[i]));
+            var plotModel = new PlotModel { Title = "Population Dynamics" };
+            predatorsSeries = new LineSeries { Title = "Predators" };
+            preysSeries = new LineSeries { Title = "Preys" };
+            plotModel.Series.Add(predatorsSeries);
+            plotModel.Series.Add(preysSeries);
+            return plotModel;
         }
 
-        // Добавляем данные из prayCountList и predatorCountList в серию для фазового портрета
-        for (int i = 0; i < prayCountList.Count; i++)
+        private PlotModel CreatePhasePlotModel()
         {
-            phaseSeries.Points.Add(new DataPoint(predatorCountList[i], prayCountList[i]));
+            var plotModel = new PlotModel { Title = "Phase Portrait" };
+            phaseSeries = new LineSeries { Title = "Predators vs Preys" };
+            plotModel.Series.Add(phaseSeries);
+            return plotModel;
         }
-
-        mainPlotModel.Series.Add(predatorsSeries);
-        mainPlotModel.Series.Add(preysSeries);
-        phasePlotModel.Series.Add(phaseSeries);
-
-        // Обновляем графики
-        _plotModel.InvalidatePlot();
-        _phaseModel.InvalidatePlot();
-    }
-    
-    private PlotModel CreateMainPlotModel()
-    {
-        var plotModel = new PlotModel { Title = "Population Dynamics" };
-        // Убираем добавление серий в модель графика
-        return plotModel;
-    }
-
-    private PlotModel CreatePhasePlotModel()
-    {
-        var plotModel = new PlotModel { Title = "Phase Portrait" };
-        // Убираем добавление серий в модель графика
-        return plotModel;
     }
 }
